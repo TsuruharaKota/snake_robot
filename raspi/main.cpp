d0dd5d9536cb46ad7fa54131d7d86eebfbc11818
TsuruharaKota <m1830@s.akashi.ac.jp>
2020-11-19 18:28:27 +0900

    fix

--- raspi/main.cpp
+++ raspi/main.cpp
@@ -2,7 +2,15 @@
 #include<cstdint>
 #include<cstring>
 #include<pigpiod_if2.h>
+#include<cmath>
+#include"joystick.h"
+#include<unistd.h>
+#include<string>
+#include<string.h>
+#include<fstream>
+#include<iomanip>
 
+using namespace std::chrono;
 using std::memcpy;
 
 #define HEAD_BYTE 0xFF
@@ -13,49 +21,30 @@ int baudrate = 115200;
 int pi = pigpio_start(0, 0);
 int serial_handle{};
 unsigned char dummy_flag{};
-//send
-float send_data[9]{};
-//receive
-uint8_t got_data{};
-uint8_t byte_now{};
-uint8_t byte[2]{};
-float receive_result[9]{};
 
-typedef struct{
-  uint16_t gyro_data;
-  uint16_t tof_data[4];
-}ReceiveFormat;
-
-void sendSerial(){
+void sendSerial(float *_send_data){
   uint8_t checksum_send = 0;
-  unsigned char data_h_h[9], data_h_l[9], data_l_h[9], data_l_l[9];
+  unsigned char data_h_h[2], data_h_l[2], data_l_h[2], data_l_l[2];
   //divide byte
-  int32_t byte_divide[9];
-  for(int i = 0; i < 9; ++i){
+  int32_t byte_divide[5];
+  for(int i = 0; i < 2; ++i){
     //float32_t to int32_t
-    memcpy(&byte_divide[i], &send_data[i], 4);
+    memcpy(&byte_divide[i], &_send_data[i], 4);
     data_h_h[i] = (byte_divide[i] >> 24) & 0xFF;
     data_h_l[i] = (byte_divide[i] >> 16) & 0xFF;
     data_l_h[i] = (byte_divide[i] >> 8) & 0xFF;
     data_l_l[i] = (byte_divide[i] >> 0) & 0xFF;
   };
-  unsigned char sendFormat[9][5] = {
+  unsigned char sendFormat[2][5] = {
     {0, data_h_h[0], data_h_l[0], data_l_h[0], data_l_l[0]},
-    {1, data_h_h[1], data_h_l[1], data_l_h[1], data_l_l[1]},
-    {2, data_h_h[2], data_h_l[2], data_l_h[2], data_l_l[2]},
-    {3, data_h_h[3], data_h_l[3], data_l_h[3], data_l_l[3]},
-    {4, data_h_h[4], data_h_l[4], data_l_h[4], data_l_l[4]},
-    {5, data_h_h[5], data_h_l[5], data_l_h[5], data_l_l[5]},
-    {6, data_h_h[6], data_h_l[6], data_l_h[6], data_l_l[6]},
-    {7, data_h_h[7], data_h_l[7], data_l_h[7], data_l_l[7]},
-    {8, data_h_h[8], data_h_l[8], data_l_h[8], data_l_l[8]},
+    {1, data_h_h[1], data_h_l[1], data_l_h[1], data_l_l[1]}
   };
   //send head byte
   serial_write_byte(pi, serial_handle, HEAD_BYTE);
   checksum_send += HEAD_BYTE;
   serial_write_byte(pi, serial_handle, STX);
   checksum_send += STX;
-  for(int i = 0; i < 9; ++i){
+  for(int i = 0; i < 2; ++i){
     for(int k = 0; k < 5; ++k){
       serial_write_byte(pi, serial_handle, sendFormat[i][k]);
       checksum_send += sendFormat[i][k];
@@ -64,9 +53,10 @@ void sendSerial(){
   //send checksum
   serial_write_byte(pi, serial_handle, checksum_send);
 }
-void receiveSerial(){
+void receiveSerial(float *_receive_result){
   uint8_t checksum_receive{};
   uint8_t receive_data[9];
+  uint8_t got_data{};
   unsigned char receiveFormat[9][5] = {
     {0, 0, 0, 0, 0},
     {1, 0, 0, 0, 0},
@@ -103,7 +93,7 @@ void receiveSerial(){
                                            | (receiveFormat[i][3] <<  8 & 0x0000FF00)
                                            | (receiveFormat[i][4] <<  0 & 0x000000FF)
           );
-          memcpy(&receive_result[i], &result[i], 4);
+          memcpy(&_receive_result[i], &result[i], 4);
         }
       }
     }
@@ -119,19 +109,58 @@ int main(int argc, char **argv) {
       printf("Serial Initialize complete\n");
     }
   }
-
   catch (int _serial_handle) {
     printf("Serial Initialize Failed\n");
     return 1;
   }
+  
+  Joystick joystick("/dev/input/js0");
+  if(!joystick.isFound()){
+    std::cerr << "open failed" << std::endl;
+  }
+  bool stop_loop = false;
+  SerialTermios serial;
+  float write_data[2]{}; //[0] = 推進速度, [1] = 旋回角度[deg]
+  float read_data[9]{};
 
-  while (1) {
-    for(int i = 0; i < 9; ++i){
-      send_data[i] += 0.1;
+  while (!stop_loop) {
+    usleep(1000);
+    JoystickEvent event;
+    if(joystick.sample(&event)){
+      if(event.isButton() && event.number == 8 && event.value == 1){
+        stop_loop = true;
+        std::cout << "exit" << std::endl;
+      }
+      if(event.isAxis()){
+        auto map = [](float x, float in_min, float in_max, float out_min, float out_max){
+          return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+        };
+        int key_num = event.number;
+        int key_val = event.value;
+        switch(key_num){
+          case 0:
+            break;
+          case 1:
+            //This program doesn't deal backwards.
+            write_data[0] = map(key_val, 0, -32767, 0, max_speed);
+            if(fabs(write_data[0]) < 1)write_data[0] = 0;
+            break;
+          case 2:
+            break;
+          case 3:
+            write_data[1] = map(key_val, -32767, 32767, 15, -15);
+            if(fabs(write_data[1]) < 7)write_data[1] = 0;
+            break;
+          case 4:
+            break;
+          case 5:
+            break;
+        }
+      }
     }
-    sendSerial();
-    receiveSerial();
-    printf("%f %f %f %f %f %f %f %f %f\n", receive_result[0], receive_result[1], receive_result[2], receive_result[3], receive_result[4], receive_result[5], receive_result[6], receive_result[7], receive_result[8]);
+    sendSerial(write_data);
+    receiveSerial(read_data);
+    printf("%f %f %f %f %f %f %f %f %f\n", read_data[0], read_data[1], read_data[2], read_data[3], read_data[4], read_data[5], read_data[6], read_data[7], read_data[8]);
   }
   serial_close(pi, serial_handle);
 }
